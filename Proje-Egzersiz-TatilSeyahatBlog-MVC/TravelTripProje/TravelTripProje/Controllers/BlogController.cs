3a35de7133a21e03b42bac1395e4b228da35c6cf
Gani Ozturk <ganioztrk@gmail.com>
2023-05-13 02:43:02 +0300

    mvc .net project

---
+++ Proje-Egzersiz-TatilSeyahatBlog-MVC/TravelTripProje/TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Entities;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly BlogYorum _blogYorum = new BlogYorum();
+        private readonly Context _context = new Context();
+        public ActionResult Index()
+        {
+            _blogYorum.Deger1 = _context.Blogs.ToList();
+            _blogYorum.Deger3 = _context.Blogs.OrderByDescending(x=>x.ID).Take(3).ToList();
+            return View(_blogYorum);
+        }
+
+        public ActionResult BlogDetay(int id)
+        {
+            _blogYorum.Deger1 = _context.Blogs.Where(x => x.ID == id).ToList();
+            _blogYorum.Deger2 = _context.Yorumlars.Where(x => x.BlogID == id).ToList();
+            return View(_blogYorum);
+        }
+
+        [HttpGet]
+        public PartialViewResult YorumYap(int id)
+        {
+            ViewBag.deger = id;
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult YorumYap(Yorumlar yorumlar)
+        {
+            _context.Yorumlars.Add(yorumlar);
+            _context.SaveChanges();
+            return PartialView();
+        }
+    }
+}
---
+++ Proje-Egzersiz-TatilSeyahatBlog-MVC/TravelTripProje/TravelTripProje/Controllers/DefaultController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Entities;
+
+namespace TravelTripProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly Context _context = new Context();
+        public ActionResult Index()
+        {
+            var values = _context.Blogs.ToList();
+            return View(values);
+        }
+        public PartialViewResult Partial1()
+        {
+            var values = _context.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(values);
+        }
+
+            
+        public PartialViewResult Partial2()
+        {
+            var values = _context.Blogs.Where(x=>x.ID == 1).ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Partial3()
+        {
+            var values = _context.Blogs.Take(10).ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Partial4()
+        {
+            var values = _context.Blogs.Take(3).ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Partial5()
+        {
+            var values = _context.Blogs.OrderByDescending(x => x.ID).Take(3).ToList();
+            return PartialView(values);
+        }
+    }
+}
